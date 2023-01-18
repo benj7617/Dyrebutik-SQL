2dd6f1cdcc772d8762209ace87966a24084b3555
benj7617 <116954841+benj7617@users.noreply.github.com>
2023-01-18 09:31:57 +0100

    Added models for databases

---
+++ Dyrehandel Database V2/Dyrehandel Database V2/ProdukterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dyrehandel_Database_V2
+{
+    public class ProduktModel
+    {
+        public int ProduktID { get; set; }
+        public string Race { get; set; }
+        public string Kategori { get; set; }
+        public int Pris { get; set; }
+    }
+}
